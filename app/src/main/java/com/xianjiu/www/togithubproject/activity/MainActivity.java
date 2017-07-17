9f1b2f70d42e70c27e669031307db2d24c246e59
lumingfang <lumingfang@parkmecn.com>
2017-07-18 00:54:18 +0800

    [创建包名]

--- app/src/main/java/com/xianjiu/www/togithubproject/activity/MainActivity.java
+++ app/src/main/java/com/xianjiu/www/togithubproject/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.xianjiu.www.togithubproject;
+package com.xianjiu.www.togithubproject.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.xianjiu.www.togithubproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
