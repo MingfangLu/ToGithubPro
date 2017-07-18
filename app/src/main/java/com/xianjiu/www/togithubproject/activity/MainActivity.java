a2a2beffee8de1fd07e75559b3a40a57c9e57dd3
lumingfang <lumingfang@parkmecn.com>
2017-07-18 13:54:10 +0800

    [创建release分支,用新开发工具Androidstudio3.0Pre]

--- app/src/main/java/com/xianjiu/www/togithubproject/activity/MainActivity.java
+++ app/src/main/java/com/xianjiu/www/togithubproject/activity/MainActivity.java
@@ -1,12 +1,11 @@
 package com.xianjiu.www.togithubproject.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.xianjiu.www.togithubproject.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
